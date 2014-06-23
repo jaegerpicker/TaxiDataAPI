8ce258a00e6469ba90b4e29c3eb8b3b9454b7357
Shawn Campbell <scampbell@cashstar.com>
2014-06-23 14:43:45 -0400

    mongoose setup

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,1 +1,14 @@
 var mongoose = require('mongoose');
+
+exports.default = {
+  mongo: function(api) {
+      return {
+        package:   'mongoose',
+        host:      'mongodb-codeart.cloudapp.net',
+        port:      27017,
+        password:  null,
+        options:   null,
+        database:  'taxi' 
+      };
+  }
+};
